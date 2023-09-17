3057fc949637f4325a3c25f2144c3fd79bc3e517
Giri <chandrarchandrashekara@gmail.com>
2023-09-16 17:06:04 -0700

    save

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function home() {
+    window.open("giri.html")
+}
+function abt() {
+    window.open("giri.abt.html")
+}
+function contus() {
+    window.open("giri.contus.html")
+}
+function usr() {
+    window.open("giri.usr.html")
+}
